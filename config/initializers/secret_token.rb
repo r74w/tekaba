409f19cab0bb5625f015519ec80a84685b17bfb4
Pinkamena Diane Pie <r7744w@gmail.com>
2013-01-11 19:28:53 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tekaba::Application.config.secret_token = '57ee0c472ed1f4b513c36b210bf1728abacb2e5a133d3e0e27ddd500a87404897756edaa2e370a79f5dbbedd584ad40586575cc66702027e2789272347fd19af'
